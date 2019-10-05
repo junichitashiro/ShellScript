bea94c5103afeaa02ddb37951dca3495d88a8fb8
junichitashiro <junichitashiro926@gmail.com>
2019-10-05 18:53:03 +0900

    ntp設定処理を追加

--- centos7_initial_setup.sh
+++ centos7_initial_setup.sh
@@ -23,6 +23,24 @@ sudo firewall-cmd --zone=public --add-service=http --permanent
 sudo firewall-cmd --zone=public --add-service=ssh --permanent
 sudo firewall-cmd --reload
 
+# 時刻同期の設定をする
+sudo yum -y install ntp
+sudo systemctl stop ntpd.service
+sudo ntpdate ntp.nict.jp
+
+sudo sed -i 's/server 0.centos.pool.ntp.org iburst/# server 0.centos.pool.ntp.org iburst/g' /etc/ntp.conf
+sudo sed -i 's/server 1.centos.pool.ntp.org iburst/# server 1.centos.pool.ntp.org iburst/g' /etc/ntp.conf
+sudo sed -i 's/server 2.centos.pool.ntp.org iburst/# server 2.centos.pool.ntp.org iburst/g' /etc/ntp.conf
+sudo sed -i 's/server 3.centos.pool.ntp.org iburst/# server 3.centos.pool.ntp.org iburst/g' /etc/ntp.conf
+
+sudo sed -i '/# server 0.centos.pool.ntp.org/i server -4 ntp.nict.jp iburst' /etc/ntp.conf
+sudo sed -i '/# server 0.centos.pool.ntp.org/i server -4 ntp.nict.jp iburst' /etc/ntp.conf
+sudo sed -i '/# server 0.centos.pool.ntp.org/i server -4 ntp.nict.jp iburst' /etc/ntp.conf
+sudo sed -i '/# server 0.centos.pool.ntp.org/i server -4 ntp.nict.jp iburst' /etc/ntp.conf
+
+sudo systemctl start ntpd.service
+sudo systemctl enable ntpd.service
+
 # マニュアルのインストール
 sudo yum -y install man
 sudo yum -y install man-pages-ja
